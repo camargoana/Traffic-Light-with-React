106270b3c368ca0c70d9b10b340e502c45411379
Ana Camargo <camargoana851@gmail.com>
2022-01-17 19:25:28 -0400

    nuevos cambios

---
+++ src/js/component/card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+ 
+
+
+export const Card = () => {
+    return (<h1> hello Card </h1>);
+      
+export default Card;
+
+};
---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+ 
+
+
+export const jumbotron = () => {
+    return (<h1> hello Jumbotron </h1>);
+      
+export default jumbotron;
+
+};
